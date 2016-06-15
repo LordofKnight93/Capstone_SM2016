b0c64b3c257e6d59c6b012f44fcf5bd3acb38dc3
Takei Fuku <doremonwithme@gmail.com>
2016-06-15 21:42:56 +0700

    Update Design (HTML,CSS)
    1- Group
    2- Project

--- WIP/User/PhucVT/iVolunteer_Web_Try_new/js/phucvt_custom.js
+++ WIP/User/PhucVT/iVolunteer_Web_Try_new/js/phucvt_custom.js
@@ -2,31 +2,6 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-$("#publictab").click(function(){
-	$("#project_nav").show();
-    $("#main_content").attr("class","col-md-9");
-})
-
-$("#dicusstab").click(function(){
-	$("#project_nav").show();
-    $("#main_content").attr("class","col-md-9");
-})
-
-$("#plantab").click(function(){
-    $("#project_nav").hide();
-    $("#main_content").attr("class","col-md-12");
-});
-
-$("#budgettab").click(function(){
-    $("#project_nav").hide();
-    $("#main_content").attr("class","col-md-12");
-});
-
-$("#gallerytab").click(function(){
-    $("#project_nav").hide();
-    $("#main_content").attr("class","col-md-12");
-});
-
 function registerformchange(){
     $("#register-form-1").css({display:"none" })
     $("#register-form-2").css({display:"block"});
